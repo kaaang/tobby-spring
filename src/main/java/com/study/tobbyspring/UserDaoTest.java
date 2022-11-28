e5cb2be26751b6ce826966311c77828e61aa301c
kaaang <kaaang@kakao.com>
2022-11-29 00:45:45 +0900

    commit

--- src/main/java/com/study/tobbyspring/UserDaoTest.java
+++ src/main/java/com/study/tobbyspring/UserDaoTest.java
@@ -1,13 +1,10 @@
 package com.study.tobbyspring;
 
-import com.study.tobbyspring.user.dao.ConnectionMaker;
-import com.study.tobbyspring.user.dao.DConnectionMaker;
+import com.study.tobbyspring.user.dao.DaoFactory;
 import com.study.tobbyspring.user.dao.UserDao;
 
 public class UserDaoTest {
     public static void main(String[] args) {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
     }
 }
